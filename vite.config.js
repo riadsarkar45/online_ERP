d947dc61aa9ad0d6990888a7da3820540fdb9d7a
Riad Sarkar <riadsarkar45@gmail.com>
2025-05-20 10:29:30 +0600

    trying again

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
-    allowedHosts: ['online-erp-5.onrender.com']
+    allowedHosts: ['.onrender.com']
   }
 });
