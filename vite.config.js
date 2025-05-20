b76c9e4c6f6c2c49b8b91bd5f99d472d4e6ca063
Riad Sarkar <riadsarkar45@gmail.com>
2025-05-20 10:19:03 +0600

    vite config host provided

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  // other configs...
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['online-erp-5.onrender.com']
+  }
+};
